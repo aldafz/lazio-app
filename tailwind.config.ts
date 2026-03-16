b1d4f00bd484825d224abf6cf8ac832bb2879d31
aldafz <rualami@gmail.com>
2026-03-16 12:56:27 +0100

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,17 +2,14 @@ import type { Config } from "tailwindcss";
 
 const config: Config> = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      // Qui potresti aggiungere i colori ufficiali per richiamarli facilmente
       colors: {
-        lazio: {
-          sky: "#87D3F8",
-          navy: "#001e3c",
-        },
+        lazio: "#87D3F8",
       },
     },
   },
