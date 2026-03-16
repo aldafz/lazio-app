64f3e1af5337036ba2dfccd7df1f7c6785b65cbd
aldafz <rualami@gmail.com>
2026-03-16 13:22:52 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import Navigation from '../components/Navigation';
 import Hero from '../components/Hero';
 import HallOfFame from '../components/HallOfFame';
 import BestEleven from '../components/BestEleven';
+import Voting from '../components/Voting'; // Importa il nuovo componente
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
       <Hero />
       <HallOfFame />
       <BestEleven />
+      <Voting /> {/* Aggiunge la sezione di voto */}
       
       {/* Footer Provvisorio */}
       <footer className="py-10 bg-black text-center border-t border-[#87D3F8]/10 text-white/40 text-xs uppercase tracking-widest px-8">
