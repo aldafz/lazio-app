58fddd3017574bcc7d9c83eec5bfb0646e3f1dc3
aldafz <rualami@gmail.com>
2026-03-16 13:38:35 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,24 @@
+"use client";
 import React from 'react';
 import Navigation from '../components/Navigation';
 import Hero from '../components/Hero';
 import HallOfFame from '../components/HallOfFame';
 import BestEleven from '../components/BestEleven';
-import Voting from '../components/Voting'; // Importa il nuovo componente
+import Voting from '../components/Voting';
 
 export default function Home() {
   return (
     <main className="scroll-smooth bg-black min-h-screen">
       <Navigation />
       <Hero />
-      <HallOfFame />
-      <BestEleven />
-      <Voting /> {/* Aggiunge la sezione di voto */}
+      <div className="space-y-0">
+        <HallOfFame />
+        <BestEleven />
+        <Voting />
+      </div>
       
-      {/* Footer Provvisorio */}
       <footer className="py-10 bg-black text-center border-t border-[#87D3F8]/10 text-white/40 text-xs uppercase tracking-widest px-8">
-        S.S. LAZIO 1900 - UNOFFICIAL SITE - PROUDLY BIANCOCELESTE
+        S.S. LAZIO 1900 - UNOFFICIAL SITE
       </footer>
     </main>
   );
