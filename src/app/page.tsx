456f469b78f27677294b8c7fe96b3c067c105a1c
aldafz <rualami@gmail.com>
2026-03-16 13:16:13 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import Navigation from '../components/Navigation';
 import Hero from '../components/Hero';
+import HallOfFame from '../components/HallOfFame';
 
 export default function Home() {
   return (
-    <main>
+    <main className="scroll-smooth">
       <Navigation />
       <Hero />
+      <HallOfFame />
       
-      {/* Sezione provvisoria per i prossimi passi */}
-      <section className="py-20 bg-black text-center">
-        <h3 className="text-lazio font-bold">PROSSIMO STEP: HALL OF FAME</h3>
+      {/* Sezione Curva Nord provvisoria */}
+      <section id="curva-nord" className="py-40 bg-[#001e3c] text-center border-t border-[#87D3F8]/10">
+        <h2 className="text-6xl font-black italic text-white uppercase opacity-20">Curva Nord</h2>
+        <p className="text-[#87D3F8] tracking-[0.5em] font-bold mt-4 italic">NON MOLLARE MAI</p>
       </section>
     </main>
   );
