502051b598343ade255374c0ad90ffcc4444d8b1
aldafz <rualami@gmail.com>
2026-03-16 12:11:43 +0100

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,1 +1,21 @@
+import type { Config } from "tailwindcss";
 
+const config: Config> = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      // Qui potresti aggiungere i colori ufficiali per richiamarli facilmente
+      colors: {
+        lazio: {
+          sky: "#87D3F8",
+          navy: "#001e3c",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
