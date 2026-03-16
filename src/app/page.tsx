f9912ab6fcf9c533e4825a818d80bbaafd2c6630
aldafz <rualami@gmail.com>
2026-03-16 13:18:02 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,19 +2,20 @@ import React from 'react';
 import Navigation from '../components/Navigation';
 import Hero from '../components/Hero';
 import HallOfFame from '../components/HallOfFame';
+import BestEleven from '../components/BestEleven';
 
 export default function Home() {
   return (
-    <main className="scroll-smooth">
+    <main className="scroll-smooth bg-black min-h-screen">
       <Navigation />
       <Hero />
       <HallOfFame />
+      <BestEleven />
       
-      {/* Sezione Curva Nord provvisoria */}
-      <section id="curva-nord" className="py-40 bg-[#001e3c] text-center border-t border-[#87D3F8]/10">
-        <h2 className="text-6xl font-black italic text-white uppercase opacity-20">Curva Nord</h2>
-        <p className="text-[#87D3F8] tracking-[0.5em] font-bold mt-4 italic">NON MOLLARE MAI</p>
-      </section>
+      {/* Footer Provvisorio */}
+      <footer className="py-10 bg-black text-center border-t border-[#87D3F8]/10 text-white/40 text-xs uppercase tracking-widest px-8">
+        S.S. LAZIO 1900 - UNOFFICIAL SITE - PROUDLY BIANCOCELESTE
+      </footer>
     </main>
   );
 }
