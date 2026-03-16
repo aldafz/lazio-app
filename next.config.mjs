1ff9156db0fb09866e5e10fc6dc12b6f1bfeeb4e
aldafz <rualami@gmail.com>
2026-03-16 12:07:57 +0100

    Update next.config.mjs

---
+++ next.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  images: { unoptimized: true },
+  // basePath: '/nome-tua-repo', // Scommenta se carichi su repo sottocartella
+};
+
+export default nextConfig;
